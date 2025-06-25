48df07bee61e19a8c867e2664076a2cd76ec759d
Ventsislav Genchev <vigour@genchev.info>
2025-06-25 18:57:58 +0300

    Add email/password login form and update tests

--- src/components/Login.js
+++ src/components/Login.js
@@ -2,7 +2,7 @@ import { useState } from "react";
 import { signInWithEmailAndPassword } from "firebase/auth";
 import { auth } from "../firebase";
 import { signInWithPopup, GoogleAuthProvider } from "firebase/auth";
-import { Button, TextField, Box, Typography, Paper } from '@mui/material';
+import { Button, TextField, Box, Typography, Paper, Alert } from '@mui/material';
 
 
 function Login() {
@@ -32,21 +32,46 @@ const googleLogin = async () => {
 
 //TextField fullWidth label="Имейл" margin="normal" />
 //        <TextField fullWidth label="Парола" type="password" margin="normal" />
-//        <Button variant="contained" fullWidth style={{ marginTop: 16 }}>Вход</Button> 
+//        <Button variant="contained" fullWidth style={{ marginTop: 16 }}>Вход</Button>
 
 
 return (
     <Box display="flex" justifyContent="center" alignItems="center" height="100vh" bgcolor="#f5f5f5">
       <Paper elevation={3} style={{ padding: 24, width: 320 }}>
         <Typography variant="h5" align="center" gutterBottom>Вход</Typography>
-	<Button
-  variant="outlined"
-  fullWidth
-  style={{ marginTop: 8 }}
-  onClick={googleLogin}
->
-  Вход с Google
-</Button>
+        <form onSubmit={login}>
+          <TextField
+            label="Имейл"
+            fullWidth
+            margin="normal"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <TextField
+            label="Парола"
+            type="password"
+            fullWidth
+            margin="normal"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          {error && (
+            <Alert severity="error" sx={{ mt: 1 }}>
+              {error}
+            </Alert>
+          )}
+          <Button variant="contained" type="submit" fullWidth sx={{ mt: 2 }}>
+            Вход
+          </Button>
+        </form>
+        <Button
+          variant="outlined"
+          fullWidth
+          sx={{ mt: 2 }}
+          onClick={googleLogin}
+        >
+          Вход с Google
+        </Button>
       </Paper>
     </Box>
   );
