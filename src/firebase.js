cecbc6891295fd7d836e2eba71e258d8d27789f6
Ventsislav Genchev <vigour@genchev.info>
2025-07-07 21:13:57 +0300

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,7 +1,9 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 
-const firebaseConfig = {
+const firebaseConfig = JSON.parse(process.env.FIREBASE_WEBAPP_CONFIG || '{}');
+
+/*const firebaseConfig = {
 
 	apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
@@ -10,8 +12,7 @@ const firebaseConfig = {
   storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
   appId: process.env.REACT_APP_FIREBASE_APP_ID
-};
-console.log("Firebase API key:", process.env.REACT_APP_FIREBASE_API_KEY);
+};*/
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 
